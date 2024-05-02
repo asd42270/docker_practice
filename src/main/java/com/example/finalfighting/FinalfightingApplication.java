21924c7e7dc84cb66b560086e059a258a2e8e5cb
이유빈 <lyb991113@gmail.com>
2024-05-03 01:22:11 +0900

    최초 커밋

---
+++ src/main/java/com/example/finalfighting/FinalfightingApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalfighting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalfightingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalfightingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalfighting/FinalfightingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalfighting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalfightingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
